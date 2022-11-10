afcf1e7ebb4cc6fcfdb54ab2cab2dda2e7b90d7d
WhomIsConcerned <117782217+WhomIsConcerned@users.noreply.github.com>
2022-11-09 19:44:34 -0500

    k

--- js/main.js
+++ js/main.js
@@ -33,28 +33,28 @@ const baseGameSpeed = 4
 const permanentUnlocks = ["Scheduling", "Shop", "Automation", "Quick task display"]
 
 const jobBaseData = {
-    "Beggar": {name: "Beggar", maxXp: 50, income: 5},
-    "Farmer": {name: "Farmer", maxXp: 100, income: 9},
-    "Fisherman": {name: "Fisherman", maxXp: 200, income: 15},
-    "Miner": {name: "Miner", maxXp: 400, income: 40},
-    "Blacksmith": {name: "Blacksmith", maxXp: 800, income: 80},
-    "Merchant": {name: "Merchant", maxXp: 1600, income: 150},
+    "Beggar": {name: "Beggar", maxXp: 5, income: 5},
+    "Farmer": {name: "Farmer", maxXp: 10, income: 9},
+    "Fisherman": {name: "Fisherman", maxXp: 20, income: 15},
+    "Miner": {name: "Miner", maxXp: 40, income: 40},
+    "Blacksmith": {name: "Blacksmith", maxXp: 80, income: 80},
+    "Merchant": {name: "Merchant", maxXp: 160, income: 150},
 
-    "Squire": {name: "Squire", maxXp: 100, income: 5},
-    "Footman": {name: "Footman", maxXp: 1000, income: 50},
-    "Veteran footman": {name: "Veteran footman", maxXp: 10000, income: 120},
-    "Knight": {name: "Knight", maxXp: 100000, income: 300},
-    "Veteran knight": {name: "Veteran knight", maxXp: 1000000, income: 1000},
-    "Elite knight": {name: "Elite knight", maxXp: 7500000, income: 3000},
-    "Holy knight": {name: "Holy knight", maxXp: 40000000, income: 15000},
-    "Legendary knight": {name: "Legendary knight", maxXp: 150000000, income: 50000},
+    "Squire": {name: "Squire", maxXp: 10, income: 5},
+    "Footman": {name: "Footman", maxXp: 100, income: 50},
+    "Veteran footman": {name: "Veteran footman", maxXp: 1000, income: 120},
+    "Knight": {name: "Knight", maxXp: 10000, income: 300},
+    "Veteran knight": {name: "Veteran knight", maxXp: 100000, income: 1000},
+    "Elite knight": {name: "Elite knight", maxXp: 750000, income: 3000},
+    "Holy knight": {name: "Holy knight", maxXp: 4000000, income: 15000},
+    "Legendary knight": {name: "Legendary knight", maxXp: 15000000, income: 50000},
 
-    "Student": {name: "Student", maxXp: 100000, income: 100},
-    "Apprentice mage": {name: "Apprentice mage", maxXp: 1000000, income: 1000},
-    "Mage": {name: "Mage", maxXp: 10000000, income: 7500},
-    "Wizard": {name: "Wizard", maxXp: 100000000, income: 50000},
-    "Master wizard": {name: "Master wizard", maxXp: 10000000000, income: 250000},
-    "Chairman": {name: "Chairman", maxXp: 1000000000000, income: 1000000},
+    "Student": {name: "Student", maxXp: 10000, income: 100},
+    "Apprentice mage": {name: "Apprentice mage", maxXp: 100000, income: 1000},
+    "Mage": {name: "Mage", maxXp: 1000000, income: 7500},
+    "Wizard": {name: "Wizard", maxXp: 10000000, income: 50000},
+    "Master wizard": {name: "Master wizard", maxXp: 1000000000, income: 250000},
+    "Chairman": {name: "Chairman", maxXp: 100000000000, income: 1000000},
 }
 
 const skillBaseData = {
