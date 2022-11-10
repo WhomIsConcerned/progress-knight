75912f48a1bbcaaf9d96b90fd2ec52022d8f77d6
WhomIsConcerned <117782217+WhomIsConcerned@users.noreply.github.com>
2022-11-09 20:56:28 -0500

    k

--- js/main.js
+++ js/main.js
@@ -64,21 +64,21 @@ const skillBaseData = {
     "Bargaining": {name: "Bargaining", maxXp: 100, effect: -0.1, description: "Expenses"},
     "Meditation": {name: "Meditation", maxXp: 100, effect: 0.1, description: "Happiness"},
 
-    "Strength": {name: "Strength", maxXp: 100, effect: 0.01, description: "Military pay"},
-    "Battle tactics": {name: "Battle tactics", maxXp: 100, effect: 0.01, description: "Military xp"},
-    "Muscle memory": {name: "Muscle memory", maxXp: 100, effect: 0.01, description: "Strength xp"},
+    "Strength": {name: "Strength", maxXp: 100, effect: 0.1, description: "Military pay"},
+    "Battle tactics": {name: "Battle tactics", maxXp: 100, effect: 0.1, description: "Military xp"},
+    "Muscle memory": {name: "Muscle memory", maxXp: 100, effect: 0.1, description: "Strength xp"},
 
-    "Mana control": {name: "Mana control", maxXp: 100, effect: 0.01, description: "T.A.A. xp"},
-    "Immortality": {name: "Immortality", maxXp: 100, effect: 0.01, description: "Longer lifespan"},
-    "Time warping": {name: "Time warping", maxXp: 100, effect: 0.01, description: "Gamespeed"},
-    "Super immortality": {name: "Super immortality", maxXp: 100, effect: 0.01, description: "Longer lifespan"},
+    "Mana control": {name: "Mana control", maxXp: 100, effect: 0.1, description: "T.A.A. xp"},
+    "Immortality": {name: "Immortality", maxXp: 100, effect: 0.1, description: "Longer lifespan"},
+    "Time warping": {name: "Time warping", maxXp: 100, effect: 0.1, description: "Gamespeed"},
+    "Super immortality": {name: "Super immortality", maxXp: 100, effect: 0.1, description: "Longer lifespan"},
 
-    "Dark influence": {name: "Dark influence", maxXp: 100, effect: 0.01, description: "All xp"},
-    "Evil control": {name: "Evil control", maxXp: 100, effect: 0.01, description: "Evil gain"},
-    "Intimidation": {name: "Intimidation", maxXp: 100, effect: -0.01, description: "Expenses"},
-    "Demon training": {name: "Demon training", maxXp: 100, effect: 0.01, description: "All xp"},
-    "Blood meditation": {name: "Blood meditation", maxXp: 100, effect: 0.01, description: "Evil gain"},
-    "Demon's wealth": {name: "Demon's wealth", maxXp: 100, effect: 0.002, description: "Job pay"},
+    "Dark influence": {name: "Dark influence", maxXp: 100, effect: 0.1, description: "All xp"},
+    "Evil control": {name: "Evil control", maxXp: 100, effect: 0.1, description: "Evil gain"},
+    "Intimidation": {name: "Intimidation", maxXp: 100, effect: -0.1, description: "Expenses"},
+    "Demon training": {name: "Demon training", maxXp: 100, effect: 0.1, description: "All xp"},
+    "Blood meditation": {name: "Blood meditation", maxXp: 100, effect: 0.1, description: "Evil gain"},
+    "Demon's wealth": {name: "Demon's wealth", maxXp: 100, effect: 0.02, description: "Job pay"},
     
 }
 
